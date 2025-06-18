885fbcb17ce54e840ddd391ecdc96b71365fd375
adithya-adee <adithya25905@gmail.com>
2025-06-18 23:53:37 +0530

    add env support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:3000';
+  const port = process.env.PORT;
+  if (!process.env.CORS_ORIGIN) {
+    console.log('NO');
+  }
   app.enableCors({
-    origin: 'http://localhost:3000', // Your frontend URL
+    origin: corsOrigin,
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(port || 3001);
 }
 void bootstrap();
